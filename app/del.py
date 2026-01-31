6004544b81415acd317249153e622f590d0006e9
Clement Fournier <fournier.clement01@gmail.com>
2026-01-31 22:16:26 +0100

    reset base update

--- app/del.py
+++ app/del.py
@@ -1,9 +1,9 @@
 from app.database import drop_all_tables, init_database
 
-print("⚠️ Suppression de toutes les tables...")
+print("Suppression de toutes les tables...")
 drop_all_tables()
 
-print("✅ Recréation des tables avec la nouvelle structure...")
+print("Recréation des tables avec la nouvelle structure...")
 init_database()
 
-print("✅ Base de données réinitialisée avec task_id!")
+print("Base de données réinitialisée avec task_id!")
